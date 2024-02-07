d1f8cadff7c58d39a825f495da152ecdd2478ac3
ednganji <ednganji01@monccnb.ca>
2024-02-07 15:39:23 -0400

    partie 1

--- src/connect.java
+++ src/connect.java
@@ -1,12 +1,14 @@
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 import java.sql.SQLException;
 
-public  class connect {
+public  class connect implements AutoCloseable {
 
 
-    public static Connection  connect1 () {
+    public static Connection  connect1()  {
          String url = "jdbc:postgresql://localhost/postgres";
         
          String user = "postgres";
@@ -22,6 +24,13 @@ public  class connect {
         }
 
         return conn;
+    }
+
+    @Override
+    public void close() throws SQLException {
+        
+            System.out.println("Connexion à la base de données fermée.");
+        
     }
 
     public static Connection connect2() {
